365f0c14cc40578d30c868c63547b7ac0b19d532
wp <wangpan_hqu@163.com>
2021-05-08 18:48:29 +0800

    left

--- src/test/Main.java
+++ src/test/Main.java
@@ -36,7 +36,7 @@ public class Main{
 		 * }
 		 */
     	System.out.println(Runtime.getRuntime().availableProcessors());
-    	System.out.println("Left");
+    	System.out.println("Left   aaa");
 	       
 	       
     	
